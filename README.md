e75dcf5fbcb9d5a0cbddbeaf159751b3d66ee515
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-21 11:09:40 -0400

    read me

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # ***Pet Guide***
 
+**APP FORECAST**
+
+This project, at the moment, is simply a 'pet guide' for someone looking for a specific type of pet. In the future, I'm hoping to turn it into more of a pet party supply app. Where users will be able to connect, share ideas, & get fun spunky party supplies for their own pet parties. 
+
+
+Although, my idea was to incorporate all three of my models into this project, It will have to be a reach goal moving forward now. 
+
+
+This project contains examples of CRUD in React for three models: Users, Pets, and Party.
+
+*Used:*
+React, Mongo, Bootstrap, and Material-UI. 
+
 
 
 
--- controllers/party.js
+++ controllers/party.js
@@ -1,62 +1,62 @@
 
-const express = require('express');
-const Party = require('../models/party');
-const Pet = require('../models/pet');
-const router = express.Router();
+// const express = require('express');
+// const Party = require('../models/party');
+// const Pet = require('../models/pet');
+// const router = express.Router();
 
-router.get('/', (req, res) => {
-    Party.find().then( (parties) => {
-        res.json(parties);
-    })
-    .catch( (err) => {
-        console.log(err);
-    })
-});
+// router.get('/', (req, res) => {
+//     Party.find().then( (parties) => {
+//         res.json(parties);
+//     })
+//     .catch( (err) => {
+//         console.log(err);
+//     })
+// });
 
-router.get('/:partyId', (req, res) => {
-    Party.findById(req.params.partyId).then( (party) => {
-        res.json(party);
-    })
-    .catch( (err) => {
-        console.log(err);
-    })
-});
+// router.get('/:partyId', (req, res) => {
+//     Party.findById(req.params.partyId).then( (party) => {
+//         res.json(party);
+//     })
+//     .catch( (err) => {
+//         console.log(err);
+//     })
+// });
 
-router.post('/', (req, res) => {
-    const newParty = new Party();
-    newParty.partyName = req.body.partyName
-    newParty.description = req.body.description
-    newParty.pet = req.body.pet
+// router.post('/', (req, res) => {
+//     const newParty = new Party();
+//     newParty.partyName = req.body.partyName
+//     newParty.description = req.body.description
+//     newParty.pet = req.body.pet
 
-    const newPet = req.body.pets.map( (pet) => {
-        return new Pet(pet);
-    })
+//     const newPet = req.body.pets.map( (pet) => {
+//         return new Pet(pet);
+//     })
 
-    newParty.pet = newPet;
+//     newParty.pet = newPet;
 
-    newParty.save().then( (party) => {
-        res.json(party);
-    }).catch( (err) => {
-        console.log(err);
-    })
-})
+//     newParty.save().then( (party) => {
+//         res.json(party);
+//     }).catch( (err) => {
+//         console.log(err);
+//     })
+// })
 
-router.put('/', (req, res) => {
-    Party.findByIdAndUpdate(req.body._id, req.body).then( (party) => {
-            console.log('Got it saved!');
-        })
-        .catch( (err) => {
-            console.log(err);
-        })
-});
+// router.put('/', (req, res) => {
+//     Party.findByIdAndUpdate(req.body._id, req.body).then( (party) => {
+//             console.log('Got it saved!');
+//         })
+//         .catch( (err) => {
+//             console.log(err);
+//         })
+// });
 
-router.get('/delete/:partyId', (req, res) => {
-    Party.findByIdAndRemove(req.params.partyId).then( (party) => {
-            console.log(`${party.partyName} was deleted`)
-        })
-        .catch( (err) => {
-            console.log(err);
-        })
-});
+// router.get('/delete/:partyId', (req, res) => {
+//     Party.findByIdAndRemove(req.params.partyId).then( (party) => {
+//             console.log(`${party.partyName} was deleted`)
+//         })
+//         .catch( (err) => {
+//             console.log(err);
+//         })
+// });
 
-module.exports = router;
+// module.exports = router;
